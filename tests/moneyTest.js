5625f4684d12b7dca015ebde03eba40b7a306961
Nancy Chaudhry <nancych14@gmail.com>
2024-11-07 19:10:59 +0500

    Create automated tests for formatCurrency

---
+++ tests/moneyTest.js
@@ -0,0 +1,31 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+
+console.log('Test suite: formatCurrency');
+
+console.log('Converts cents into dollars');
+if (formatCurrency(2095) === '20.95') {
+  console.log('Test passed')
+} else {
+  console.log('Test failed')
+}
+
+console.log('Works with zero');
+if (formatCurrency(0) === '0.00') {
+  console.log('Test passed')
+} else {
+  console.log('Test failed')
+}
+
+console.log('Rounds up to nearest cent');
+if (formatCurrency(2000.5) === '20.01') {
+  console.log('Test passed')
+} else {
+  console.log('Test failed')
+}
+
+console.log('Rounds down to nearest cent');
+if (formatCurrency(2000.4) === '20.00') {
+	console.log('Test passed');
+} else {
+	console.log('Test failed');
+}
